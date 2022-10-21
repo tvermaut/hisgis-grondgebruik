5accd2058be8fe9b39eae23a9b380eb002031106
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-21 17:31:59 +0200

    itemnr

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
+var itemnr = 0;
+
 $(function() {
     fetch('https://data.hisgis.nl/w/api.php?action=wbgetentities&ids=Q101&format=json')
         .then(response => response.json())
-        .then(data => {$("#lijst").html(verwerkWB(data.entities.Q101))})
+        .then(data => {$("#lijst").appendChild(verwerkWB(data.entities.Q101))})
   });
 
   async function verwerkWB(j){
     // P33 = ongebouwd
     var ul1 = document.createElement("ul");
+    ul1.attr("id",itemnr);
+    document.appendChild(ul1);
+    var ul1_id = itemnr;
+    itemnr++;
     for(let i of j.claims.P33){
         let lii = await checkWBi(i.mainsnak.datavalue.value.id);
-        if(lii){ul1.appendChild(lii);}
+        if(lii){$('#'+ul1_id).appendChild(lii);}
         if(i.hasOwnProperty("qualifiers") && i.qualifiers.hasOwnProperty("P36")){
             var ul2 = document.createElement("ul");
+            ul2.attr("id",itemnr);
+            var ul2_id = itemnr;
+            itemnr++;
+            $('#'+ul1_id).appendChild(ul2);
             for(let q of i.qualifiers.P36){ // P36 = tariefsoort
                 let ul2li = await checkWBi(q.datavalue.value.id);
-                if(ul2li){ul2.appendChild(ul2li);}
+                if(ul2li){$('#'+ul2_id).appendChild(ul2li);}
                 }
-            ul1.appendChild(ul2);
+                
             }
         }
         return ul1
@@ -31,6 +41,8 @@ async function checkWBi(wbid){
             let q = data.entities[Object.getOwnPropertyNames(data.entities)[0]];
             //console.log(q);
             let li = document.createElement("li");
+            li.attr("id",itemnr);
+            itemnr++;
             li.innerHTML = q.labels.nl.value + (q.hasOwnProperty("aliases") && q.aliases.hasOwnProperty("nl") ? ': ' + (q.aliases.nl.map(getNL)).join(', ') : '');
             console.log(li);
             return li
