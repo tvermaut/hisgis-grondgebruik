6e7251e2862ecd013ab1dcb3e158f35138a26956
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-21 17:39:39 +0200

    append

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ var itemnr = 0;
 $(function() {
     fetch('https://data.hisgis.nl/w/api.php?action=wbgetentities&ids=Q101&format=json')
         .then(response => response.json())
-        .then(data => {$("#lijst").appendChild(verwerkWB(data.entities.Q101))})
+        .then(data => {$("#lijst").append(verwerkWB(data.entities.Q101))})
   });
 
   async function verwerkWB(j){
     // P33 = ongebouwd
     var ul1 = document.createElement("ul");
     ul1.setAttribute("id",itemnr);
-    $('#lijst').appendChild(ul1);
+    $('#lijst').append(ul1);
     var ul1_id = itemnr;
     itemnr++;
     for(let i of j.claims.P33){
         let lii = await checkWBi(i.mainsnak.datavalue.value.id);
-        if(lii){$('#'+ul1_id).appendChild(lii);}
+        if(lii){$('#'+ul1_id).append(lii);}
         if(i.hasOwnProperty("qualifiers") && i.qualifiers.hasOwnProperty("P36")){
             var ul2 = document.createElement("ul");
             ul2.setAttribute("id",itemnr);
             var ul2_id = itemnr;
             itemnr++;
-            $('#'+ul1_id).appendChild(ul2);
+            $('#'+ul1_id).append(ul2);
             for(let q of i.qualifiers.P36){ // P36 = tariefsoort
                 let ul2li = await checkWBi(q.datavalue.value.id);
-                if(ul2li){$('#'+ul2_id).appendChild(ul2li);}
+                if(ul2li){$('#'+ul2_id).append(ul2li);}
                 }
                 
             }
