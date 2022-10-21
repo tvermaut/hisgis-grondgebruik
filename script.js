5ea6017adcce2e83066f843c4cb84aec96dd8ad8
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-21 17:57:23 +0200

    async

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var itemnr = 0;
 
-$(function() {
+$(async function() {
     fetch('https://data.hisgis.nl/w/api.php?action=wbgetentities&ids=Q101&format=json')
         .then(response => response.json())
         .then(data => {await $("#lijst").append(verwerkWB(data.entities.Q101))})
